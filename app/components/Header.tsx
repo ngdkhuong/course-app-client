3ec394212ef0dc45c2ee25f9fe9a562f2d016a8e
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-01-02 17:05:13 +0700

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -38,9 +38,9 @@ const Header: FC<Props> = ({ activeItem, setOpen }) => {
             <div
                 className={`${
                     active
-                        ? 'dark:bg-opacity-50 dark:bg-gradient-to-b  dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500'
-                        : 'w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow'
-                }`}
+                        ? 'dark:bg-opacity-50 dark:bg-gradient-to-b  dark:from-gray-900 dark:to-black dark:border-[#ffffff1c] shadow-xl'
+                        : 'dark:border-[#ffffff1c] dark:shadow'
+                } fixed top-0 left-0 w-full h-[80px] z-[80] border-b transition duration-500`}
             >
                 <div className="w-[95%] md:w-[92%] m-auto py-2 h-full">
                     <div className="w-full h-[80px] flex items-center justify-between p-3">
---
+++ app/components/Route/Hero.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+type Props = {};
+
+const Hero: FC<Props> = (props) => {
+    return (
+        <div className="w-full md:flex items-center">
+            <div className="absolute top-[100px] md:top-[unset] lg:h-[700px] lg:w-[700px] md:h-[600px] md:w-[600px] h-[50vh] w-[50vh]"></div>
+            <div className="md:w-[40%] flex md:min-h-screen items-center justify-end pt-[70px] md:pt-[0] z-[10]">
+                <Image
+                    src={require('../../../public/images/image-hero.png')}
+                    alt=""
+                    width={1000}
+                    height={1000}
+                    className="object-contain md:max-w-[90%] w-[90%] lg:max-w-[85%] h-[auto] z-[10]"
+                />
+            </div>
+            <div className="md:w-[60%] flex flex-col items-center md:text-left mt-[150px]">
+                <h2 className="dark:text-white text-black text-[30px] px-3 w-full md:text-[70px] font-[600] font-Josefin py-2 md:!w-[78%]">
+                    Khóa học lập trình C++ từ cơ bản đến nâng cao.
+                </h2>
+                <br />
+                <p className="dark:text-white text-black font-Josefin font-[600] text-[18px] lg:!w-[55%] md:!w-[78%]">
+                    Đạt được nền tảng kỹ thuật lập trình cực kì vững chắc và tư duy trong việc giải quyết vấn đề. Thành
+                    thạo kiến thức về lập trình Hướng Đối Tượng và vận dụng kiến thức này để giải quyết các bài toán
+                    thực tế.
+                </p>
+                <br />
+                <br />
+                <div className="lg:w-[55%] md:w-[78%] w-[90%] h-[50px] relative">
+                    <button type="submit" title="Xem Chi Tiết"></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
